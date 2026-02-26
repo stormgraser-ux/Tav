4fd16f2b3b093766297908f52252e5cfb7b096a1
Stormgraser <stormgraser@gmail.com>
2026-02-25 19:06:04 -0800

    auto: .gitignore 19:06:04

--- legacy/run_autopilot_v2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-"C:\Users\Owner\AppData\Local\Programs\Python\Python312\python.exe" autopilot_v2.py
-pause
--- legacy/run_calibrate.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-"C:\Users\Owner\AppData\Local\Programs\Python\Python312\python.exe" calibrate.py
-pause
